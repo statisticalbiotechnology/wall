c1b040ab8ef16b0d3fd8bbc461b5f1933f51ef22
pbjarterot <46728406+SweLG@users.noreply.github.com>
2020-08-31 12:06:45 +0200

    Update single_cell_pipeline.sh

--- single_cell/src/single_cell_pipeline.sh
+++ single_cell/src/single_cell_pipeline.sh
@@ -1,29 +1,25 @@
-exec &> pipeline.log
-echo Welcome to the single cell pathway analysis/Sunburst plotting pipeline!;
-echo To start, we will download the datafiles and put them together in a dataframe we will call full_df.csv;
-echo There will also be a pheno_df file containing informartion about which cells are dress and which are hv;
-echo but first, lets install the required modules
-
-python3 -m pip install -r requirements.txt
-Rscript requirements.R
-python3 file_preparation.py
-
-
-FILE=../data/lfc_df.csv
+echo "Welcome to the single cell pathway analysis/Sunburst plotting pipeline!";
+echo "To start, we will download the datafiles and put them together in a dataframe we will call full_df.csv";
+echo "There will also be a pheno_df file containing informartion about which cells are dress and which are hv";
+echo "but first, let's install the required modules";
+python3 -m pip install -r requirements.txt;
+Rscript requirements.R;
+python3 file_preparation.py;
+FILE=../data/lfc_df.csv;
 if [ -e "$FILE" ]; then
-	echo differential expression file already exist, proceeding with pathway analysis;
+	echo "differential expression file already exist, proceeding with pathway analysis"
 	python3 prepare_idea.py
 	Rscript iDEA.R;
 else
-	echo differential expresssion file does not exist, proceeding with differential expression;
+	echo "differential expresssion file does not exist, proceeding with differential expression"
 	Rscript differential_expression.R;
 	python3_prepare_idea.py
 	Rscript iDEA.R
 fi
-echo Lets gather the results and convert them to a csv containing the -log10 q-values
+echo "Lets gather the results and convert them to a csv containing the -log10 q-values";
 
 
-python3 results.py
-echo Lets make the sunburst plots
+python3 results.py;
+echo "Lets make the sunburst plots";
 
-python3 run_sunburst.py
+python3 run_sunburst.py;
